4740961fe26fb0ec727442665ab9d80d0cd12352
victormayowa185 <edoziealaefuna3000@gmail.com>
2026-04-28 16:51:06 +0100

    improve the scanning for speed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['tesseract.js']
-  }
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("tesseract.js")) return "tesseract";
+          if (id.includes("gsap")) return "gsap";
+        },
+      },
+    },
+  },
+});
